930a716a46b3c675202a3d601fabcb91a642d278
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-06-07 01:49:21 -0300

    Estrutura inicial projeto spring-boot

---
+++ basketball-app-back-end/src/main/java/com/vx/basketball/app/AppBasketballApplication.java
@@ -0,0 +1,13 @@
+package com.vx.basketball.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBasketballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBasketballApplication.class, args);
+	}
+
+}
---
+++ basketball-app-back-end/src/test/java/com/vx/basketball/app/AppBasketballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vx.basketball.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBasketballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
